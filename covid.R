37506bb4abe4891dd1fa51a0b7c218601790fef4
Austin Nguyen <nguyenau@ohsu.edu>
2020-03-08 21:39:46 -0700

    updated, clean for mhcflurry code

--- covid.R
+++ covid.R
@@ -1,3 +1,14 @@
 setwd("~/Documents/Coding/covid/covid19")
-hla_binding <- read.csv("covid_hla.csv", header=FALSE)
-hla_binding_500 <- hla_binding[as.integer(hla_binding$V3) < 500,]
+hla_binding <- read.csv("covid_hla_filt.csv")
+gsub('.{4}$', '', hla_binding$file_name) -> hla_binding$file_name
+sub('..........', '', hla_binding$file_name) -> hla_binding$file_name
+hla_binding_500 <- hla_binding[as.integer(hla_binding$ic50) < 500,]
+hla_binding_500 <- hla_binding_500[!is.na(as.integer(hla_binding_500$ic50)),]
+colnames(hla_binding_500)[1] <- "allele"
+write.csv(hla_binding_500, file="covid_500.csv", row.names = FALSE)
+library(ggplot2)
+
+count <- hla_binding_500$allele
+t
+
+
