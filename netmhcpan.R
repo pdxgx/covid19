35326cb5e9ac041d15566123c6e7b24641fa4fa1
reidt03 <thompsre@ohsu.edu>
2020-03-19 23:52:01 -0700

    modifies HLA proteome presentation plots

--- netmhcpan.R
+++ netmhcpan.R
@@ -31,7 +31,7 @@ covid_recode <- hla_cleavednet_filt %>%
   arrange(desc(less_than_50))
 covid_recode$allele <- reorder(covid_recode$allele,covid_recode$allele,FUN=length)
 
-allele_freqs <- as.data.frame(read.csv("allele_freqs.csv", header=FALSE))
+allele_freqs <- as.data.frame(read.csv("allele_freqs.csv", header=TRUE))
 colnames(allele_freqs) <- c("allele", "frequency")
 allele_freqs$class <- ifelse(grepl("A", allele_freqs$allele), 'A', ifelse(grepl("B", allele_freqs$allele), 'B', 'C'))
 allele_freqs$allele <- paste("HLA-", allele_freqs$allele, sep="")
@@ -46,32 +46,35 @@ merged_allele_df <- merged_allele_df[merged_allele_df$Freq>0,]
 a <- ggplot(covid_recode, aes(x = allele, y = occ, fill = class, alpha = as.integer(less_than_50)))+ 
   geom_bar(position="stack", stat="identity")+
   theme_classic()+
-  theme(axis.title.y=element_blank()) + 
-  scale_fill_manual(values=c('#1B9E77', '#D95F02', '#7570B3'))+
+  ylab("")+
+  xlab("HLA alleles")+
+  scale_fill_manual(values=c('#1B9E77', '#D95F02', '#7570B3'),name = "Gene", labels = c("HLA-A", "HLA-B", "HLA-C"))+
   coord_flip()+
-  ggtitle("covid <500nm- netmhcpan")+
+  ggtitle("SARS-CoV-2 Presentation")+
   scale_y_discrete(expand=c(0,0))+
-  scale_alpha(range = c(0.6, 1), guide = FALSE)+
-  theme(axis.ticks = element_line(size = .2), text = element_text(size=20))
+  scale_alpha(range = c(0.5, 1), guide = FALSE)+
+  theme(axis.ticks = element_line(size = .2), text = element_text(size=20), axis.text.y=element_blank(), legend.key.size=unit(1,"cm"), panel.border=element_rect(colour="black", fill=NA, size=1))
 
 b <- ggplot(merged_allele_df, aes(x = reorder(allele,Freq), y = as.numeric(as.character(frequency)), fill = class))+
   geom_bar(position="stack", stat="identity")+
   theme_classic() + scale_x_discrete(position='top')+
   theme(axis.title.y=element_blank(), axis.text.y=element_blank())+
-  #theme(axis.title.y=element_blank())+
+  ylab("Global allele frequency")+
   scale_fill_manual(values=c('#1B9E77', '#D95F02', '#7570B3'))+
   coord_flip()+
-  ggtitle("Allele frequencies") + scale_y_reverse(expand=(c(0,0)))+
+  #ggtitle("Global allele frequency")+
+  scale_y_reverse(expand=(c(0,0)))+
+  scale_alpha(range = c(0.5, 1), guide = FALSE)+
   theme(legend.position = "none")+
-  theme(axis.ticks = element_line(size = .2))
+  theme(axis.ticks = element_line(size = .2), text = element_text(size=20),panel.border=element_rect(colour="black", fill=NA, size=1))
 
 #grid.arrange(b,a, ncol=2)
 gA <- ggplotGrob(a)
 gB <- ggplotGrob(b)
-
+pdf(file="~/Desktop/coronavirus/plots/HLA_distrib.pdf",height=7,width=9)
 grid::grid.newpage()
 grid::grid.draw(cbind(gB, gA))
-
+dev.off()
 
 
 
@@ -88,35 +91,38 @@ merged_allele_df <- merge(new_allele_df, allele_freqs_filt, by="allele", all=TRU
 merged_allele_df <- merged_allele_df[merged_allele_df$Freq>0,]
 
 
-#merged_allele_df <- read.csv("nick2.csv")
 a <- ggplot(sars_recode, aes(x = allele, y = occ, fill = class, alpha = as.integer(less_than_50)))+ 
   geom_bar(position="stack", stat="identity")+
   theme_classic()+
-  theme(axis.title.y=element_blank()) + 
-  scale_fill_manual(values=c('#1B9E77', '#D95F02', '#7570B3'))+
+  ylab("")+
+  xlab("HLA alleles")+
+  scale_fill_manual(values=c('#1B9E77', '#D95F02', '#7570B3'),name = "Gene", labels = c("HLA-A", "HLA-B", "HLA-C"))+
   coord_flip()+
-  ggtitle("SARS <500nm- netmhcpan")+
+  ggtitle("SARS-CoV Presentation")+
   scale_y_discrete(expand=c(0,0))+
-  scale_alpha(range = c(0.6, 1), guide = FALSE)+
-  theme(axis.ticks = element_line(size = .2), text = element_text(size=20))
+  scale_alpha(range = c(0.5, 1), guide = FALSE)+
+  theme(axis.ticks = element_line(size = .2), text = element_text(size=20), axis.text.y=element_blank(), legend.key.size=unit(1,"cm"), panel.border=element_rect(colour="black", fill=NA, size=1))
 
 b <- ggplot(merged_allele_df, aes(x = reorder(allele,Freq), y = as.numeric(as.character(frequency)), fill = class))+
   geom_bar(position="stack", stat="identity")+
   theme_classic() + scale_x_discrete(position='top')+
   theme(axis.title.y=element_blank(), axis.text.y=element_blank())+
-  #theme(axis.title.y=element_blank())+
+  ylab("Global allele frequency")+
   scale_fill_manual(values=c('#1B9E77', '#D95F02', '#7570B3'))+
   coord_flip()+
-  ggtitle("Allele frequencies") + scale_y_reverse(expand=c(0,0))+
+  #ggtitle("Global allele frequency")+
+  scale_y_reverse(expand=(c(0,0)))+
+  scale_alpha(range = c(0.5, 1), guide = FALSE)+
   theme(legend.position = "none")+
-  theme(axis.ticks = element_line(size = .2))
+  theme(axis.ticks = element_line(size = .2), text = element_text(size=20),panel.border=element_rect(colour="black", fill=NA, size=1))
 
 #grid.arrange(b,a, ncol=2)
 gA <- ggplotGrob(a)
 gB <- ggplotGrob(b)
-
+pdf(file="~/Desktop/coronavirus/plots/HLA_distrib_SARS.pdf",height=7,width=9)
 grid::grid.newpage()
 grid::grid.draw(cbind(gB, gA))
+dev.off()
 
 
 
