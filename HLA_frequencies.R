a8835d7c4ec4d01f30d4891da5e66179ef2254b5
reidt03 <thompsre@ohsu.edu>
2020-03-19 22:09:57 -0700

    adds plotting for Appendix (maps)

--- HLA_frequencies.R
+++ HLA_frequencies.R
@@ -12,6 +12,8 @@ require(rworldmap)
 alleles.outfile <- "HLA-freqs.rda"
 iso3.outfile <- "HLA-ISO3-freqs.rda"
 global.outfile <- "HLA-freqs-global.rda"
+MHCpan.alleles.infile <- "netmhcpan_allele_list.txt"
+plotdir <- "plots"
 
 #--------------------
 # get allele frequency data
@@ -171,14 +173,24 @@ HLAgeoPlot <- function(HLA, outdir=NULL,unified.freq=FALSE) {
 #----------
 # generate figures for paper
 #----------
-pdf(file="plots/best+worst_HLA_all_peptides.pdf", width=9,height=8.5)
+MHCpan_alleles <- as.character(unlist(read.csv(MHCpan.alleles.infile, header=TRUE))) %>%
+	sub(pattern="HLA[-]([A-C])",replacement="\\1*")
+
+# Figure XXX
+pdf(file=paste0(plotdir, "/best+worst_HLA_all_peptides.pdf"), width=9,height=8.5)
 layout(matrix(1:6,nr=3,nc=2))
 par(mar=c(3.1,0.1,2.1,0.1))
 HLAgeoPlot(c("A*02:06", "A*02:07", "B*15:03", "B*46:01", "C*12:03", "C*01:02"))
 dev.off()
 
-pdf(file="plots/best+worst_HLA_conserved_peptides.pdf", width=9,height=8.5)
+# Supplementary Figure XXX
+pdf(file=paste0(plotdir, "/best+worst_HLA_conserved_peptides.pdf"), width=9,height=8.5)
 layout(matrix(1:6,nr=3,nc=2))
 par(mar=c(3.1,0.1,2.1,0.1))
 HLAgeoPlot(c("A*02:06", "A*02:07", "B*08:01", "B*46:01", "C*12:03", "C*01:02"))
 dev.off()
+
+# Appendix XXX
+for (HLA in MHCpan_alleles) {
+	HLAgeoPlot(HLA, outdir=plotdir)
+}
